1e556cd09effe51cea8203983ef0b7bb13321e78
CodeLionX <sebastian.schmidl@t-online.de>
2019-04-23 18:26:24 +0200

    WIP: work on typed column collections

--- src/main/scala/com/github/codelionx/dodo/parsing/TypedColumnProcessor.scala
+++ src/main/scala/com/github/codelionx/dodo/parsing/TypedColumnProcessor.scala
@@ -1,36 +1,63 @@
 package com.github.codelionx.dodo.parsing
 
+import com.github.codelionx.dodo.types.typedColumns
+import com.github.codelionx.dodo.types.typedColumns.{TypedColumn, TypedColumnBuffer}
 import com.univocity.parsers.common.ParsingContext
 import com.univocity.parsers.common.processor.RowProcessor
 
+import scala.collection.mutable.ArrayBuffer
+
 class TypedColumnProcessor(numberOfTypeInferingRows: Int = 20) extends RowProcessor {
 
-//  val columns: Array[]
+  var columns: Array[TypedColumnBuffer] = _
   val untypedRowBuffer: Array[Array[String]] = Array.ofDim(numberOfTypeInferingRows)
   var inferrer: TypeInferrer = _
-  var nTypedRows: Int = 0
+  var columnsIndex: Int = 0
+  var untypedRowBufferIndex: Int = 0
 
   override def processStarted(context: ParsingContext): Unit = {}
 
   override def rowProcessed(row: Array[String], context: ParsingContext): Unit = {
-    if(nTypedRows < numberOfTypeInferingRows) {
+    if(untypedRowBufferIndex < numberOfTypeInferingRows) {
       // lazy initialization of type inferrer to use the row size
       if (inferrer == null) {
         inferrer = new IterativeTypeInferrer(row.length)
       }
 
       inferrer.refreshTypesFromRow(row)
-      untypedRowBuffer(nTypedRows) = row
-      nTypedRows += 1
-    } else {
+      untypedRowBuffer(untypedRowBufferIndex) = row
+      untypedRowBufferIndex += 1
 
+    } else {
+      // type inferring finished, continue with reading the buffer again and parse it to the right types
       if (untypedRowBuffer.nonEmpty) {
         val types = inferrer.columnTypes
-        untypedRowBuffer.map( row => {
-          types.indices.map( i =>
+
+        // initialize column arrays
+        columns =  Array.ofDim(types.length)
+        types.indices.foreach(i => {
+          val dataType = types(i)
+          columns(i) = typedColumns.bufferFromDataType(dataType)
+        })
+
+        // fill column arrays with buffered data
+        untypedRowBuffer.foreach( row => {
+          val values = types.indices.map( i =>
             types(i).parse(row(i))
           )
+          for(j <- values.indices) {
+            val tBuffer = columns(columnsIndex)
+            tBuffer.append(values(j))
+          }
+          columnsIndex += 1
         })
+
+        println("Buffered and reparsed columns:")
+        println(s"Current row index: $columnsIndex")
+        println(columns.zip(inferrer.columnTypes).map{
+          case (xs, t) => t.toString + xs.backingArray.mkString(", ")
+        }.mkString("\n  ")
+        )
       }
     }
   }
---
+++ src/main/scala/com/github/codelionx/dodo/types/typedColumns.scala
@@ -0,0 +1,102 @@
+package com.github.codelionx.dodo.types
+
+import java.time.{LocalDateTime, ZonedDateTime}
+
+import scala.collection.mutable.ArrayBuffer
+
+object typedColumns {
+  sealed trait TypedColumn {
+    type T <: Any
+
+    def backingArray: Array[T]
+
+    def apply(index: Int): T
+  }
+
+  trait TypedColumnBuffer extends TypedColumn {
+    type T <: Any
+
+    def toTypedColumn: TypedColumn
+
+    def apply(index: Int): T
+
+    def append[U <: T](elems: U*): Unit
+  }
+
+
+  def bufferFromDataType(t: DataType): TypedColumnBuffer = t match {
+    case NullType => StringColumnBuffer.empty
+    case StringType => StringColumnBuffer.empty
+    case _ => StringColumnBuffer.empty
+//    case LongType => ArrayBuffer.empty[Long]
+//    case DoubleType => ArrayBuffer.empty[Double]
+//    case LocalDateType(_) => ArrayBuffer.empty[LocalDateTime]
+//    case ZonedDateType(_) => ArrayBuffer.empty[ZonedDateTime]
+  }
+
+  def fromDataType(t: DataType): TypedColumn = t match {
+    case NullType => StringColumn.empty
+    case StringType => StringColumn.empty
+//    case LongType => LongColumn.empty
+//    case DoubleType => DoubleColumn.empty
+//    case LocalDateType(_) => LocalDateColumn.empty
+//    case ZonedDateType(_) => ZonedDateColumn.empty
+  }
+
+  final class StringColumn private (arr: Array[String]) extends TypedColumn {
+    override type T = String
+
+    override def backingArray: Array[String] = arr
+
+    override def apply(index: Int): String = arr(index)
+  }
+
+  final class StringColumnBuffer extends TypedColumnBuffer {
+    override type T = String
+
+    private val buffer: ArrayBuffer[String] = ArrayBuffer.empty
+//    private val dataType: DataType = StringType
+
+    override def toTypedColumn: TypedColumn = new StringColumn(buffer.toArray)
+
+    override def backingArray: Array[String] = buffer.toArray
+
+    override def apply(index: Int): String = buffer(index)
+
+    override def append[U <: T](elems: U*): Unit = buffer.append(elems: _*)
+
+//    def append(elems: )
+  }
+
+  object StringColumn {
+    val empty: StringColumn = new StringColumn(Array.empty)
+  }
+
+  object StringColumnBuffer {
+    val empty: StringColumnBuffer = new StringColumnBuffer
+  }
+
+//  type LongColumn = Array[Long]
+//
+//  object LongColumn {
+//    val empty: LongColumn = Array.empty[Long]
+//  }
+//
+//  type DoubleColumn = Array[Double]
+//
+//  object DoubleColumn {
+//    val empty: DoubleColumn = Array.empty[Double]
+//  }
+//
+//  type LocalDateColumn = Array[LocalDateTime]
+//
+//  object LocalDateColumn {
+//    def empty: LocalDateColumn = Array.empty[LocalDateTime]
+//  }
+//
+//  type ZonedDateColumn = Array[ZonedDateTime]
+//
+//  object ZonedDateColumn {
+//    def empty: ZonedDateColumn = Array.empty[ZonedDateTime]
+//  }
+}
