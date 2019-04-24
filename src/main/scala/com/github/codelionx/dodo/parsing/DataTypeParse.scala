5d461292dd4d85532a17ad0003274087def1b727
CodeLionX <sebastian.schmidl@t-online.de>
2019-04-24 22:37:59 +0200

    finished implementation of type class with all trait implementations
    BUT:
    the implicit-inference does not work with generic types of classes, as used in TypedColumnBuilder. This means
    that this approach is not feasable for us in this use case.

--- src/main/scala/com/github/codelionx/dodo/parsing/DataTypeParse.scala
+++ src/main/scala/com/github/codelionx/dodo/parsing/DataTypeParse.scala
@@ -1,34 +1,77 @@
 package com.github.codelionx.dodo.parsing
 
-import com.github.codelionx.dodo.types.{DataType, LongType, StringType}
+import java.time.temporal.TemporalAccessor
+import java.time.{Instant, LocalDateTime, ZoneId, ZonedDateTime}
+
+import com.github.codelionx.dodo.types._
 
 import scala.util.Try
 
 
 /**
-  * Type class 
+  * Type class that adds a `parse()` method to [[com.github.codelionx.dodo.types.DataType]]s and details how to
+  * parse the different data types.
   *
-  * @see https://scalac.io/typeclasses-in-scala for how to write type classes
-  * @tparam V
+  * @see [[https://scalac.io/typeclasses-in-scala]] for how to write type classes
+  * @tparam V output type, the string should be parsed to
   */
-trait Parse[V] {
-  def parse(in: String): V
+trait DataTypeParse[V] {
+
+  def parse(dataType: DataType[V])(in: String): V
+
 }
 
-object Parse {
+object DataTypeParse {
 
-  def apply[V](implicit p: Parse[V]): Parse[V] = p
+  /**
+    * Pulls in the correct instance for the supplied type by an implicit.
+    *
+    * @param p implicit implementation of the [[DataTypeParse]] trait
+    */
+  def apply[V](implicit p: DataTypeParse[V]): DataTypeParse[V] = p
 
-//  def parse[V : Parse](dataType: DataType[V])(in: String): V = Parse[V].parse(dataType)(in)
+  // could be used to parse with `DataTypeParse.parse(dType)("something")`
+  def parse[V: DataTypeParse](dataType: DataType[V])(in: String): V = DataTypeParse[V].parse(dataType)(in)
 
-  implicit val longCanParse: Parse[Long] = (in: String) => Try {
-    in.toLong
-  }.getOrElse(0L)
+  /**
+    * Ops for this type class. Specifies, where this type class can be applied to (on [[DataType]]s).
+    */
+  implicit class DataTypeParseOps[V: DataTypeParse](dataType: DataType[V]) {
+    def parse(value: String): V = DataTypeParse[V].parse(dataType)(value)
+  }
 
-  implicit val stringCanParse: Parse[String] = identity[String]
+  // implementations:
 
-  implicit class ParseOps[V : Parse](dataType: DataType[V]) {
-    def parse(value: String): V = Parse[V].parse(value)
+  implicit val stringCanParse: DataTypeParse[String] = new DataTypeParse[String] {
+    override def parse(dataType: DataType[String])(in: String): String = in
+  }
+
+  implicit val nullCanParse: DataTypeParse[Null] = new DataTypeParse[Null] {
+    override def parse(dataType: DataType[Null])(in: String): Null = null
+  }
+
+  implicit val longCanParse: DataTypeParse[Long] = new DataTypeParse[Long] {
+    override def parse(dataType: DataType[Long])(in: String): Long = Try {
+      in.toLong
+    }.getOrElse(0L)
+  }
+
+  implicit val doubleCanParse: DataTypeParse[Double] = new DataTypeParse[Double] {
+    override def parse(dataType: DataType[Double])(in: String): Double = Try {
+      in.toDouble
+    }.getOrElse(.0)
+  }
+
+  implicit val zonedDateTimeCanParse: DataTypeParse[ZonedDateTime] = new DataTypeParse[ZonedDateTime] {
+    override def parse(dataType: DataType[ZonedDateTime])(in: String): ZonedDateTime = Try {
+      dataType.asInstanceOf[ZonedDateType].format.parse[ZonedDateTime](in, (temp: TemporalAccessor) => ZonedDateTime.from(temp))
+    }.getOrElse(ZonedDateTime.ofInstant(Instant.EPOCH, ZoneId.systemDefault()))
+  }
+
+  implicit val localDateTimeCanParse: DataTypeParse[LocalDateTime] = new DataTypeParse[LocalDateTime] {
+    override def parse(dataType: DataType[LocalDateTime])(in: String): LocalDateTime = Try {
+      dataType.asInstanceOf[LocalDateType].format.parse[LocalDateTime](in, (temp: TemporalAccessor) => LocalDateTime.from(temp))
+    }.getOrElse(LocalDateTime.ofInstant(Instant.EPOCH, ZoneId.systemDefault()))
   }
 }
 
@@ -36,7 +79,7 @@ object Parse {
 object Main {
 
   def main(args: Array[String]): Unit = {
-    import Parse._
+    import DataTypeParse._
 
     val lType = LongType
     println(lType.parse("1234567788"))
