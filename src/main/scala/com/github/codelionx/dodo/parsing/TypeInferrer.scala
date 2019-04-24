754604f49463b314ba23e488e892d56539ea53c1
CodeLionX <sebastian.schmidl@t-online.de>
2019-04-24 23:35:17 +0200

    docu for type inferrer

--- src/main/scala/com/github/codelionx/dodo/parsing/TypeInferrer.scala
+++ src/main/scala/com/github/codelionx/dodo/parsing/TypeInferrer.scala
@@ -5,6 +5,11 @@ import com.github.codelionx.dodo.types._
 
 object TypeInferrer {
 
+  /**
+    * Infers the type of the value and returns it as an instance of [[DataType]].
+    *
+    * @note The value is not transformed! You can do this by using [[DataType.parse]].
+    */
   def inferType(value: String): DataType[_ <: Any] = {
     if (NullType.isNull(value))
       return NullType
@@ -28,28 +33,39 @@ object TypeInferrer {
 
 }
 
+/**
+  * Infers the type from incoming rows (parsed from CSV) for each column.
+  */
 trait TypeInferrer {
 
-  def refreshTypesFromRow(row: Array[String]): Unit
+  def inferTypesFromRow(row: Array[String]): Unit
 
+  /**
+    * Returns the inferred column types as instance of [[DataType]].
+    */
   def columnTypes: Seq[DataType[Any]]
 
 }
 
+/**
+  * Type inferrer that iteratively refines the column types each time called. The type order (which type is more
+  * specific than another one) is defined in [[com.github.codelionx.dodo.types.DateType$.ordering]].
+  *
+  * @param numberOfColumns used to initialize the column data type representation
+  */
 class IterativeTypeInferrer(numberOfColumns: Int) extends TypeInferrer {
 
-  private val types: Array[DataType[Any]] = Array.apply( (0 until numberOfColumns).map(_ => NullType.asInstanceOf[DataType[Any]]): _* )
+  private val types: Array[DataType[Any]] =
+    Array.apply((0 until numberOfColumns).map(_ => NullType.asInstanceOf[DataType[Any]]): _*)
 
   /**
-    * Columns in a row must arrive in the same order every time!
-    *
-    * @param row
+    * @note Columns in a row must arrive in the same order every time!
     */
-  override def refreshTypesFromRow(row: Array[String]): Unit = {
+  override def inferTypesFromRow(row: Array[String]): Unit = {
     val newTypes = row.map(TypeInferrer.inferType)
 
-    for(i <- row.indices) {
-      if(types(i) != newTypes(i) && types(i) < newTypes(i)) {
+    for (i <- row.indices) {
+      if (types(i) != newTypes(i) && types(i) < newTypes(i)) {
         types(i) = newTypes(i).asInstanceOf[DataType[Any]]
       }
     }
