ceeaf47dce259eea3a51de4cacf4b9a4755aad67
CodeLionX <sebastian.schmidl@t-online.de>
2019-05-02 11:15:57 +0200

    fix shutdown (case-sensitive member roles)

--- src/main/scala/com/github/codelionx/dodo/Main.scala
+++ src/main/scala/com/github/codelionx/dodo/Main.scala
@@ -2,13 +2,14 @@ package com.github.codelionx.dodo
 
 import akka.cluster.Cluster
 
+
 object Main {
 
   val actorSystemName = "dodo-system"
 
   def main(args: Array[String]): Unit = {
-    val masterRole = "MASTER"
-    val slaveRole = "SLAVE"
+    val masterRole = "master"
+    val slaveRole = "slave"
     val host = "localhost"
     val port = 7877
 
@@ -25,7 +26,12 @@ object Main {
 
     cluster.registerOnMemberUp {
       println("Cluster up")
-      system.terminate()
+
+      // shutting down
+      println("nothing to do -- shutting down")
+      // system.terminate()
+      // or
+      cluster.down(cluster.selfAddress)
     }
   }
 }
