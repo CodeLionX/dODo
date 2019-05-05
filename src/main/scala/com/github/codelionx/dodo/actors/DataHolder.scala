b4f74c2d2fac99cd53677532d7cb23f6b40d3dc5
CodeLionX <sebastian.schmidl@t-online.de>
2019-05-05 16:24:23 +0200

    use data loader to load and parse a data file into memory

--- src/main/scala/com/github/codelionx/dodo/actors/DataHolder.scala
+++ src/main/scala/com/github/codelionx/dodo/actors/DataHolder.scala
@@ -1,6 +1,8 @@
 package com.github.codelionx.dodo.actors
 
 import akka.actor.{Actor, ActorLogging, Props}
+import com.github.codelionx.dodo.parsing.CSVParser
+import com.github.codelionx.dodo.types.TypedColumn
 
 
 object DataHolder {
@@ -8,6 +10,16 @@ object DataHolder {
   val name = "dataholder"
 
   def props(): Props = Props[DataHolder]
+
+  case class LoadData(localFilename: String) extends Serializable
+
+  // following messages should only be used within the actor system
+  /**
+    * Message to request the data reference. It is returned as [[com.github.codelionx.dodo.actors.DataHolder.DataRef]]
+    */
+  case object GetDataRef
+
+  case class DataRef(relation: Array[TypedColumn[Any]])
 
 }
 
@@ -24,10 +36,22 @@ class DataHolder extends Actor with ActorLogging {
   override def postStop(): Unit =
     log.info(s"Stopping $name")
 
-  override def receive: Receive = {
-    // TODO: get dataSource -> load data
-    // TODO: Other nodes need data -> send it
+  override def receive: Receive = uninitialized
+
+  def uninitialized: Receive = {
+    case LoadData(localFilename) =>
+      val data = CSVParser.parse(localFilename)
+      log.info(s"Loaded data from $localFilename. $name is ready")
+      context.become(dataReady(data))
+
     case _ => log.info("Unknown message received")
   }
 
+  def dataReady(relation: Array[TypedColumn[Any]]): Receive = {
+    case GetDataRef =>
+      log.info(s"Serving data to ${sender.path}")
+      sender ! DataRef(relation)
+
+    case _ => log.info("Unknown message received")
+  }
 }
--- src/main/scala/com/github/codelionx/dodo/actors/SystemCoordinator.scala
+++ src/main/scala/com/github/codelionx/dodo/actors/SystemCoordinator.scala
@@ -1,6 +1,7 @@
 package com.github.codelionx.dodo.actors
 
-import akka.actor.{Actor, ActorLogging, Props}
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
+import com.github.codelionx.dodo.actors.DataHolder.{DataRef, GetDataRef, LoadData}
 
 
 object SystemCoordinator {
@@ -9,13 +10,17 @@ object SystemCoordinator {
 
   def props(dataSource: String): Props = Props(new SystemCoordinator(dataSource))
 
+  case object Initialize
+
 }
 
 class SystemCoordinator(dataSource: String) extends Actor with ActorLogging {
 
   import SystemCoordinator._
 
-  // TODO: get dataHolder to read data from dataSource
+
+  val dataHolder: ActorRef = context.actorOf(DataHolder.props(), DataHolder.name)
+
   // TODO: setup workerManager to start extracting ODs once data is read
   // TODO: create result Collector
 
@@ -30,6 +35,27 @@ class SystemCoordinator(dataSource: String) extends Actor with ActorLogging {
     log.info(s"Stopping $name")
 
   override def receive: Receive = {
+    case Initialize =>
+      log.info("Preparing for OD discovery")
+      log.info("  Loading and parsing data")
+      dataHolder ! LoadData(dataSource)
+
+      log.info("  Creating initial search space")
+      log.info("  ...")
+
+      // test if data passing works
+      // ---
+      log.info("Testing data passing ...")
+      dataHolder ! GetDataRef
+
+    case DataRef(data) =>
+      log.info("... data passing successful:")
+      println(data.map(col => col.dataType + ": " + col.toArray.mkString(", ")).mkString("\n"))
+
+      log.info("shutting down")
+      context.stop(self)
+    // ---
+
     // TODO
     case _ => log.info("Unknown message received")
   }
