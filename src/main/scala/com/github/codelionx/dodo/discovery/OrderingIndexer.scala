be7d14fa84858d82d305491664f76f1e8ac126a8
CodeLionX <sebastian.schmidl@t-online.de>
2019-05-07 23:55:55 +0200

    small improvements

--- src/main/scala/com/github/codelionx/dodo/discovery/OrderingIndexer.scala
+++ src/main/scala/com/github/codelionx/dodo/discovery/OrderingIndexer.scala
@@ -1,21 +1,20 @@
 package com.github.codelionx.dodo.discovery
 
-import com.github.codelionx.dodo.types.{DataType, LongType, StringType, TypedColumn}
+import com.github.codelionx.dodo.types.{DataType, TypedColumn}
+
 
 object OrderingIndexer {
 
-  def orderedIndices(dataset: Array[TypedColumn[_ <: Any]]): Seq[Int] = {
+  def orderedIndices(dataset: Array[TypedColumn[_ <: Any]]): Seq[Int] =
     recSorting(dataset, dataset.head.indices, 0)
-  }
 
-  def recSorting(dataset: Array[TypedColumn[_ <: Any]], sortIndices: Seq[Int], colIndex: Int): Seq[Int] = {
+  private def recSorting(dataset: Array[TypedColumn[_ <: Any]], sortIndices: Seq[Int], colIndex: Int): Seq[Int] = {
     // get new values from dataset of the current slice
     val indexedCol = Array.ofDim[(Any, Int)](sortIndices.length)
     val column = dataset(colIndex)
     for (i <- sortIndices.indices) {
       val index = sortIndices(i)
       indexedCol(i) = column(index) -> index
-      () => ()
     }
 
     // sort slice according to this column
@@ -28,8 +27,8 @@ object OrderingIndexer {
         if (!sortedIndexedCol(start)._1.equals(sortedIndexedCol(i)._1)) {
           if (start != i - 1) {
             val indexRange = start until i
-            val currentSlice = sortedIndexedCol.slice(start, i).map(_._2)
-            val updatedSlice = recSorting(dataset, currentSlice, colIndex + 1)
+            val currentView = sortedIndexedCol.view(start, i).map(_._2)
+            val updatedSlice = recSorting(dataset, currentView, colIndex + 1)
             for (j <- updatedSlice.indices) {
               val index = indexRange(j)
               sortedIndexedCol(index) = sortedIndexedCol(index)._1 -> updatedSlice(j)
@@ -42,14 +41,9 @@ object OrderingIndexer {
     sortedIndexedCol.map(_._2)
   }
 
+  @inline
   private def sort(colIndices: Array[(Any, Int)], dataType: DataType[_]): Array[(Any, Int)] = {
-//    val data = dataType match {
-//      case StringType =>
-//        colIndices.asInstanceOf[Array[(String, Int)]].sortBy(_._1)
-//      case LongType =>
-//        colIndices.asInstanceOf[Array[(Long, Int)]].sortBy(_._1)
-//    }
-//    data.asInstanceOf[Array[(Any, Int)]]
+    // work around scala implicit search for an Ordering[Any] ;)
     implicit val tOrdering: Ordering[Any] = dataType.ordering.asInstanceOf[Ordering[Any]]
     colIndices.sorted
   }
