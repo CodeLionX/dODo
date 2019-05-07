7e220dff1a93d5290156e126c80cf8c477508636
CodeLionX <sebastian.schmidl@t-online.de>
2019-05-07 17:56:25 +0200

    use new factory methods

--- src/test/scala/com/github/codelionx/dodo/discovery/OrderingIndexerSpec.scala
+++ src/test/scala/com/github/codelionx/dodo/discovery/OrderingIndexerSpec.scala
@@ -7,17 +7,12 @@ import org.scalatest.{Matchers, WordSpec}
 class OrderingIndexerSpec extends WordSpec with Matchers {
 
   "The OrderingIndexer" should {
+
     "order a dataset" in {
       val dataset: Array[TypedColumn[_ <: Any]] = Array(
-        (TypedColumnBuilder(StringType)
-          += "C" += "A" += "C" += "Z" += "C"
-          ).toTypedColumn,
-        (TypedColumnBuilder(LongType)
-          += 8 += 20 += 5 += 24 += 5
-          ).toTypedColumn,
-        (TypedColumnBuilder(StringType)
-          += "e" += "d" += "b" += "c" += "a"
-          ).toTypedColumn
+        TypedColumnBuilder.from("C", "A", "C", "Z", "C"),
+        TypedColumnBuilder.from(8L, 20L, 5L, 24L, 5L),
+        TypedColumnBuilder.from("e", "d", "b", "c", "a")
       )
       OrderingIndexer.orderedIndices(dataset) shouldEqual Seq(1, 4, 2, 0, 3)
     }
