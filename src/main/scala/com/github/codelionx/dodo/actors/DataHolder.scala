ed5a4515a8f45e824efcb6f2babc72c0250cb0d6
CodeLionX <sebastian.schmidl@t-online.de>
2019-05-29 00:50:47 +0200

    accept connections on a side channel and handle data requests with the akka streams api

--- src/main/scala/com/github/codelionx/dodo/actors/DataHolder.scala
+++ src/main/scala/com/github/codelionx/dodo/actors/DataHolder.scala
@@ -1,9 +1,17 @@
 package com.github.codelionx.dodo.actors
 
 import akka.actor.{Actor, ActorLogging, Props}
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Tcp.{IncomingConnection, ServerBinding}
+import akka.stream.scaladsl.{Source, Tcp}
 import com.github.codelionx.dodo.Settings
+import com.github.codelionx.dodo.Settings.DefaultValues
+import com.github.codelionx.dodo.actors.DataStreamServant.HandleConnection
 import com.github.codelionx.dodo.parsing.CSVParser
 import com.github.codelionx.dodo.types.TypedColumn
+
+import scala.concurrent.Future
+import scala.language.postfixOps
 
 
 object DataHolder {
@@ -16,13 +24,14 @@ object DataHolder {
 
   case object DataLoaded
 
-  // following messages should only be used within the actor system
   /**
     * Message to request the data reference. It is returned as [[com.github.codelionx.dodo.actors.DataHolder.DataRef]]
     */
   case object GetDataRef
 
-  case class DataRef(relation: Array[TypedColumn[Any]])
+  case object DataNotReady
+
+  case class DataRef(relation: Array[TypedColumn[Any]]) extends Serializable
 
 }
 
@@ -30,8 +39,12 @@ class DataHolder extends Actor with ActorLogging {
 
   import DataHolder._
 
-  private val settings = Settings(context.system)
 
+  implicit val mat: ActorMaterializer = ActorMaterializer()(context)
+
+  private val settings = Settings(context.system)
+  private val source: Source[IncomingConnection, Future[ServerBinding]] =
+    Tcp()(context.system).bind(DefaultValues.HOST, DefaultValues.PORT + 1000)
 
   override def preStart(): Unit = {
     log.info(s"Starting $name")
@@ -44,20 +57,34 @@ class DataHolder extends Actor with ActorLogging {
   override def receive: Receive = uninitialized
 
   def uninitialized: Receive = {
+
     case LoadData(localFilename) =>
       val data = CSVParser(settings.parsing).parse(localFilename)
       log.info(s"Loaded data from $localFilename. $name is ready")
-      context.become(dataReady(data))
       sender ! DataLoaded
 
-    case _ => log.info("Unknown message received")
+      source.runForeach { connection =>
+        log.info(s"Received connection from ${connection.remoteAddress}")
+        context.actorOf(DataStreamServant.props(data)) ! HandleConnection(connection)
+      }
+
+      context.become(dataReady(data))
+
+    case GetDataRef =>
+      log.warning(s"Request to serve data from uninitialized $name")
+      sender ! DataNotReady
+
+    case msg => log.info(s"Unknown message received: $msg")
   }
 
   def dataReady(relation: Array[TypedColumn[Any]]): Receive = {
+    case LoadData(_) =>
+      sender ! DataLoaded
+
     case GetDataRef =>
       log.info(s"Serving data to ${sender.path}")
       sender ! DataRef(relation)
 
-    case _ => log.info("Unknown message received")
+    case msg => log.info(s"Unknown message received: $msg")
   }
 }
---
+++ src/main/scala/com/github/codelionx/dodo/actors/DataStreamServant.scala
@@ -0,0 +1,134 @@
+package com.github.codelionx.dodo.actors
+
+import akka.actor.Status.Failure
+import akka.actor.{Actor, ActorLogging, Props}
+import akka.pattern.pipe
+import akka.serialization.SerializationExtension
+import akka.stream.QueueOfferResult.{Dropped, Enqueued, QueueClosed}
+import akka.stream.scaladsl.Tcp.IncomingConnection
+import akka.stream.scaladsl.{Flow, Keep, Sink, Source, SourceQueueWithComplete}
+import akka.stream.{ActorMaterializer, OverflowStrategy, QueueOfferResult}
+import akka.util.ByteString
+import com.github.codelionx.dodo.actors.DataStreamServant._
+import com.github.codelionx.dodo.types.TypedColumn
+
+import scala.concurrent.Future
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
+
+object DataStreamServant {
+
+  def props(data: Array[TypedColumn[Any]]): Props = Props(new DataStreamServant(data))
+
+  case class HandleConnection(con: IncomingConnection)
+
+  case object GetDataOverStream
+
+  case class DataRefOverStream(data: Array[TypedColumn[Any]])
+
+  case object StreamInit
+
+  case object StreamACK
+
+  case object StreamComplete
+
+}
+
+
+class DataStreamServant(data: Array[TypedColumn[Any]]) extends Actor with ActorLogging {
+
+  implicit private val mat: ActorMaterializer = ActorMaterializer()
+  private val serialization = SerializationExtension(context.system)
+
+  ////////////// write test message in bytes into file `out`
+  //  val writer = new FileOutputStream("out")
+  //  writer.write(serialization.serialize(GetDataOverStream).get)
+  //  writer.close()
+  //////////////
+
+  private val actorConnector = Flow.fromSinkAndSourceMat(
+    Sink.actorRefWithAck[GetDataOverStream.type](self, StreamInit, StreamACK, StreamComplete),
+    Source.queue[DataRefOverStream](1, OverflowStrategy.backpressure)
+      .map(serialization.serialize)
+      .map {
+        case scala.util.Success(msg) => ByteString.fromArray(msg)
+        case scala.util.Failure(f) => throw f
+      }
+  )(Keep.right)
+
+  private val handler = Flow[ByteString]
+    .map(bytes => serialization.deserialize(bytes.toArray, GetDataOverStream.getClass))
+    .map {
+      case scala.util.Success(msg) => msg
+      case scala.util.Failure(f) => throw f
+    }
+    .viaMat(actorConnector)(Keep.right)
+
+  override def receive: Receive = {
+    case HandleConnection(connection) =>
+      val sourceQueue = connection.handleWith(handler)
+      context.become(streamRequest(sourceQueue))
+  }
+
+  def streamRequest(source: SourceQueueWithComplete[DataStreamServant.DataRefOverStream]): Receive = {
+
+    case StreamInit =>
+      log.info("Stream initialized")
+      sender ! StreamACK
+
+    case GetDataOverStream =>
+      log.info("Received request for data over stream")
+      source.offer(DataRefOverStream(data))
+      val offerFuture: Future[QueueOfferResult] = source.offer(DataRefOverStream(data))
+      sender ! StreamACK
+
+      import context.dispatcher
+      offerFuture pipeTo self
+      context.become(handleEnqueing(source))
+
+    case Failure(f) =>
+      log.error(s"Error processing incoming request: $f")
+      source.fail(f)
+
+    case msg =>
+      log.error(s"Received unknown request: $msg")
+      source.fail(new RuntimeException("Unknown request"))
+      context.become(waitingForClose())
+  }
+
+  def handleEnqueing(source: SourceQueueWithComplete[DataStreamServant.DataRefOverStream]): Receive = {
+
+    case Enqueued =>
+      log.info("Data was send")
+      source.complete()
+      context.become(waitingForClose())
+
+    case QueueClosed =>
+      log.info("Stream closed")
+      context.stop(self)
+
+    case Failure(cause) =>
+      log.error(s"Streaming failed to deliver data, because $cause")
+      source.fail(cause)
+    //      context.stop(self)
+
+    case Dropped =>
+      log.info("Data was dropped, trying again")
+      context.system.scheduler.scheduleOnce(1 second) {
+        val offerFuture: Future[QueueOfferResult] = source.offer(DataRefOverStream(data))
+        import context.dispatcher
+        offerFuture pipeTo self
+        context.become(handleEnqueing(source))
+      }(scala.concurrent.ExecutionContext.Implicits.global)
+
+  }
+
+  def waitingForClose(): Receive = {
+
+    case StreamComplete =>
+      log.info("Stream completed! Stopping servant")
+      sender ! StreamACK
+      context.stop(self)
+  }
+}
