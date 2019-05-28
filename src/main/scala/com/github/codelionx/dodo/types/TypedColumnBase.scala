6df4efc47ef65b883f34f394c27d493e0645166f
CodeLionX <sebastian.schmidl@t-online.de>
2019-05-28 20:10:13 +0200

    make TypedColumn serializable

---
+++ src/main/scala/com/github/codelionx/dodo/types/TypedColumnBase.scala
@@ -0,0 +1,30 @@
+package com.github.codelionx.dodo.types
+
+import scala.reflect.ClassTag
+
+
+/**
+  * Represents a column of a dataset associated with a specific type. The cell data is stored in the correct
+  * (primitive) type.
+  *
+  * @tparam T the data type
+  */
+abstract class TypedColumnBase[T <: Any](implicit ev: ClassTag[T]) {
+
+  protected val tag: ClassTag[T] = ev
+
+  /**
+    * Returns the [[com.github.codelionx.dodo.types.DataType]] associated with this column.
+    */
+  def dataType: DataType[T]
+
+  /**
+    * Returns the backing array of this `TypedColumn`.
+    */
+  def array: Array[T]
+
+  /**
+    * Column name
+    */
+  def name: String
+}
