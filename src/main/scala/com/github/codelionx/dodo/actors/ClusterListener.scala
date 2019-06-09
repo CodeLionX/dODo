7dc0caa16e78a2f4c6dc2ae6629927e166a77f2a
CodeLionX <sebastian.schmidl@t-online.de>
2019-06-09 21:57:30 +0200

    implement and refactor left neighbour getter

--- src/main/scala/com/github/codelionx/dodo/actors/ClusterListener.scala
+++ src/main/scala/com/github/codelionx/dodo/actors/ClusterListener.scala
@@ -1,9 +1,11 @@
 package com.github.codelionx.dodo.actors
 
-import akka.actor.{Actor, ActorLogging, Props}
+import akka.actor.{Actor, ActorLogging, Props, RootActorPath}
 import akka.cluster.ClusterEvent._
-import akka.cluster.{Cluster, Member, UniqueAddress}
+import akka.cluster.{Cluster, Member}
 import com.github.codelionx.dodo.actors.ClusterListener.{GetLeftNeighbour, LeftNeighbour}
+
+import scala.util.Try
 
 
 object ClusterListener {
@@ -14,24 +16,25 @@ object ClusterListener {
 
   case object GetLeftNeighbour
 
-  case class LeftNeighbour(address: UniqueAddress)
+  case class LeftNeighbour(address: RootActorPath)
 
 }
 
 
 class ClusterListener extends Actor with ActorLogging {
 
-  implicit val memberOrder: Ordering[Member] = Member.ageOrdering
+  implicit private val memberOrder: Ordering[Member] = Member.ageOrdering
 
   private val cluster = Cluster(context.system)
+  private val selfMember = cluster.selfMember
 
-  cluster.selfMember
-
-  override def preStart(): Unit =
+  override def preStart(): Unit = {
+    Reaper.watchWithDefault(self)
     cluster.subscribe(self, initialStateMode = InitialStateAsEvents,
       classOf[MemberUp], classOf[MemberRemoved],
       classOf[UnreachableMember], classOf[ReachableMember]
     )
+  }
 
   override def postStop(): Unit =
     cluster.unsubscribe(self)
@@ -52,20 +55,27 @@ class ClusterListener extends Actor with ActorLogging {
       log.info(s"$node detected reachable again")
 
     case GetLeftNeighbour =>
-      val selfMember = cluster.selfMember
-      val selfPosition = members.indexOf(selfMember)
+      getLeftNeighbour(members)
+        .map(member =>
+          sender ! LeftNeighbour(RootActorPath(member.address))
+        )
+        .recover(sendError)
+  }
 
-      val leftMember = selfPosition match {
-        case -1 =>
-          log.error("our node is not up yet??")
-          throw new RuntimeException("Could not find self node in the member list!")
-        case 0 =>
-          members.last
-        case i =>
-          members(i - 1)
-      }
+  private def sendError: PartialFunction[Throwable, Unit] = {
+    case error => sender ! akka.actor.Status.Failure(error)
+  }
 
-      sender ! LeftNeighbour(leftMember.uniqueAddress)
-
+  private def getLeftNeighbour(members: Seq[Member]): Try[Member] = Try {
+    members.indexOf(selfMember) match {
+      case -1 =>
+        log.error("our node is not up yet??")
+        log.info(s"current member list: ${members.mkString(", ")}")
+        throw new RuntimeException("Could not find self node in the member list!")
+      case 0 =>
+        members.last
+      case i =>
+        members(i - 1)
+    }
   }
 }
