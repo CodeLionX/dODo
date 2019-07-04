141e7232d2195aeb7e64f16502586f9d90962e07
CodeLionX <sebastian.schmidl@t-online.de>
2019-07-04 14:50:52 +0200

    adopt type inferrer test to reflect parsing changes

--- src/test/scala/com/github/codelionx/dodo/parsing/TypeInferrerSpec.scala
+++ src/test/scala/com/github/codelionx/dodo/parsing/TypeInferrerSpec.scala
@@ -18,22 +18,21 @@ class TypeInferrerSpec extends WordSpec with Matchers {
       TypeInferrer.inferType(Long.MaxValue.toString).shouldEqual(LongType)
     }
 
-    "infer datetimes without time zone" in {
-      TypeInferrer.inferType("2011-12-03T10:15:30").shouldEqual(LocalDateType(IsoDateTimeFormat))
-
-    }
-
     "infer datetimes with time zone" in {
       TypeInferrer.inferType("2019-12-27T22:15:30+02:00").shouldEqual(ZonedDateTimeType(IsoDateTimeFormat))
       TypeInferrer.inferType("Tue, 3 Jun 2008 11:05:30 GMT").shouldEqual(ZonedDateTimeType(RFC_1123_Format))
       TypeInferrer.inferType("10 Jun 2019 08:05:30 GMT").shouldEqual(ZonedDateTimeType(RFC_1123_Format))
     }
 
+    "infer datetimes without time zone" in {
+      TypeInferrer.inferType("2011-12-03T10:15:30").shouldEqual(LocalDateTimeType(IsoDateTimeFormat))
+    }
+
     "infer dates" in {
       // dates are always local (without zone)
       TypeInferrer.inferType("2011-12-03").shouldEqual(LocalDateType(IsoDateFormat))
       TypeInferrer.inferType("2011-12-03+01:00").shouldEqual(LocalDateType(IsoDateFormat))
-      TypeInferrer.inferType("20111203").shouldEqual(LocalDateType(BasicFormat))
+      TypeInferrer.inferType("01/28/1997").shouldEqual(LocalDateType(CustomUSDateFormat))
     }
 
     "infer strings" in {
@@ -46,6 +45,11 @@ class TypeInferrerSpec extends WordSpec with Matchers {
       TypeInferrer.inferType("").shouldEqual(NullType)
       TypeInferrer.inferType("null").shouldEqual(NullType)
       TypeInferrer.inferType("?").shouldEqual(NullType)
+    }
+
+    // bugfix guides
+    "not infer 0-padded number with 6 digits as date" in {
+      TypeInferrer.inferType("000023").shouldEqual(LongType)
     }
   }
 }
