4807285eb8cd39b3e3b92ddc677802db24c1ee8c
CodeLionX <sebastian.schmidl@t-online.de>
2019-08-11 15:10:50 +0200

    prepare start script for multi-node setup

--- benchmarking/start.sh
+++ benchmarking/start.sh
@@ -4,14 +4,14 @@
 java -jar \
   -Xmx40g -Xms40g \
   -Dcom.github.codelionx.dodo.output-file=results.txt \
-  -Dcom.github.codelionx.dodo.workers=1 \
+  -Dcom.github.codelionx.dodo.workers=8 \
   -Dakka.loglevel=\"INFO\" \
   -Dlogback.configurationFile=file:logback.xml \
   dodo.jar \
   --input-file=data/flight_1k_stripped10.csv \
   --has-header=true \
-  --host=$(hostname) \
-  --seed-host=$(hostname) \
+  --host=${HOSTNAME} \
+  --seed-host=${SEED_HOSTNAME} \
   --port=7877 \
   --seed-port=7877 \
   > metrics.csv
