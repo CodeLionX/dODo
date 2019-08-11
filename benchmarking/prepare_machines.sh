447c26ae811b9004986cf24fe8e4e15af760a44d
CodeLionX <sebastian.schmidl@t-online.de>
2019-08-11 15:11:03 +0200

    add prepare script for nodes

--- benchmarking/prepare_machines.sh
+++ benchmarking/prepare_machines.sh
@@ -1,22 +1,29 @@
 #!/usr/bin/env bash
 
 # create odin node hostnames
-nodes=""
-odin_end=8
-for ((i=1;i<=odin_end;i++)); do
-  nodes="${nodes} odin0${i}"
-done
+nodes="172.16.64.61 172.16.64.62 172.16.64.63 172.16.64.64 172.16.64.65 172.16.64.66 172.16.64.67 172.16.64.68"
+#nodes="172.16.64.62 172.16.64.63 172.16.64.64 172.16.64.65 172.16.64.66 172.16.64.67 172.16.64.68"
+seed_node="172.16.64.62"
+#odin_end=8
+#for ((i=1;i<=odin_end;i++)); do
+#  nodes="${nodes} odin0${i}"
+#done
 
 files="start.sh clean.sh pack.sh record_stats.sh logback.xml dodo.jar"
 
 for node in ${nodes}; do
   echo "Preparing node ${node}"
   # ensure directory
-  ssh student@${node} 'mkdir -p ~/dodo'
+  ssh student@"${node}" 'mkdir -p ~/dodo/data'
 
   # copy over files using ssh
 
   # actually use globbing and word splitting here
   # shellcheck disable=SC2086
-  scp ${files} student@${node}:~/dodo/
+  rsync ${files} student@"${node}":~/dodo/
+  rsync ../data/flight_1k.csv ../data/flight_1k_stripped10.csv student@"${node}":~/dodo/data/
+
+  # set node ip and seed node ip
+  ssh student@"${node}" "sed -i 's/\${HOSTNAME}/${node}/g' ~/dodo/start.sh; sed -i 's/\${SEED_HOSTNAME}/${seed_node}/g' ~/dodo/start.sh"
+
 done
